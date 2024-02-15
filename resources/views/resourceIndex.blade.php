90f3334dc8aed21fd643877a55e90481bf7941d4
NathaRuiz <nruizm98@gmail.com>
2024-02-15 13:10:30 +0100

    create resource index and its view,controller and route

---
+++ resources/views/resourceIndex.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>resources</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="pt-4 text-center">My Resources</h1>
+    @if($resources->isEmpty())
+        <p>The table is empty</p>
+    @else
+        @foreach($resources as $resource)
+            <div class="d-flex justify-content-between align-items-center">
+                <div>
+                    <h2>{{ $resource->title }}</h2>
+                    <p><strong>Author:</strong> {{ $resource->author }}</p>
+                </div>
+            </div>
+        @endforeach
+    @endif
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ResourceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +21,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/resource', [ResourceController::class,'index'])->name('resource.index');
 
