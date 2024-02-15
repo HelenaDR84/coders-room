7cbc2f42f9d478ef458ad57379279e4bbff38596
maymurph <airam.g.jordan@gmail.com>
2024-02-15 12:01:57 +0100

    Add relationships in Models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
+    protected $guarded = []; //indicamos que campos de la tabla no se puede manipular a traves del modelo con guarded
+    public function resources(): HasMany{
+        return $this->hasMany(Resource::class,'category_id');
+    }
 }
--- app/Models/ExtraResource.php
+++ app/Models/ExtraResource.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ExtraResource extends Model
 {
     use HasFactory;
+    protected $guarded = []; //indicamos que campos de la tabla no se puede manipular a traves del modelo con guarded
+    public function resource1(): HasMany{
+        return $this->hasMany(Resource::class,'extra_resource_id1');
+    }
+    
+    public function resource2(): HasMany{
+        return $this->hasMany(Resource::class,'extra_resource_id2');
+    }
+    
+    public function resource3(): HasMany{
+        return $this->hasMany(Resource::class,'extra_resource_id3');
+    }
 }
