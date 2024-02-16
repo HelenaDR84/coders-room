98d68cb7cb8506ba7f01ca2191aced49ae967400
NathaRuiz <nruizm98@gmail.com>
2024-02-16 14:35:45 +0100

    manage errors

--- resources/views/resourceCreate.blade.php
+++ resources/views/resourceCreate.blade.php
@@ -16,6 +16,9 @@
                 <option value="{{ $type->id }}">{{ $type->title }}</option>
             @endforeach
         </select>
+        @error('type_id')
+        <div class="text-red-500">{{ $message }}</div>
+        @enderror
 
         <label for="category_id">Category:</label>
         <select name="category_id" >
@@ -24,21 +27,39 @@
                 <option value="{{ $category->id }}"> {{ $category->category_title }}</option>
             @endforeach
         </select>
+        @error('category_id')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
 
         <label for="title">Title:</label>
         <input type="text" name="title"  value="{{ old('title') }}">
+        @error('title')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
 
         <label for="author">Author:</label>
         <input type="text" name="author"  value="{{ old('author') }}">
+        @error('author')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
 
         <label for="recording">Recording:</label>
         <input type="text" name="recording"  value="{{ old('recording') }}">
+        @error('recording')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
 
         <label for="presentation">Presentation:</label>
         <input type="text" name="presentation"  value="{{ old('presentation') }}">
+        @error('presentation')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
 
         <label for="repository">Repository:</label>
         <input type="text" name="repository"  value="{{ old('repository') }}">
+        @error('repository')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
 
         <label for="extra_resource_id1">Extra Resource 1:</label>
         <select name="extra_resource_id1" >
@@ -47,6 +68,9 @@
                 <option value="{{ $extraResource->id }}"> {{ $extraResource->extra_res_title }}</option>
             @endforeach
         </select>
+        @error('extra_resource_id1')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
 
         <label for="extra_resource_id2">Extra Resource 2:</label>
         <select name="extra_resource_id2" >
@@ -55,6 +79,9 @@
                 <option value="{{ $extraResource->id }}"> {{ $extraResource->extra_res_title }}</option>
             @endforeach
         </select>
+        @error('extra_resource_id2')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
 
         <label for="extra_resource_id3">Extra Resource 3:</label>
         <select name="extra_resource_id3" >
@@ -63,6 +90,9 @@
                 <option value="{{ $extraResource->id }}"> {{ $extraResource->extra_res_title }}</option>
             @endforeach
         </select>
+        @error('extra_resource_id3')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
 
         <button type="submit" class="">Crear</button>
         <a href="{{ route('resource.index') }}" class="">Cancelar</a>
