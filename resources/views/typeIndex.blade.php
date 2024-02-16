035993690e2a32ee294d5b2988a90acdc6cfea44
Helena de Rosa <149380974+HelenaDR84@users.noreply.github.com>
2024-02-16 15:42:53 +0100

    create typeIndex and controller

---
+++ resources/views/typeIndex.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Types</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="pt-4 text-center">My Types</h1>
+    @if($types->isEmpty())
+        <p>The table is empty</p>
+    @else
+        @foreach($types as $type)
+            <div class="d-flex justify-content-between align-items-center mb-3 p-3">
+                <h2>{{ $type->title }}</h2>
+            
+            </div>
+        @endforeach  
+     @endif       
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+
 use App\Http\Controllers\ResourceController;
 use App\Models\Resource;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\TypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +32,27 @@ Route::get('/resource/show/{resource}',[ResourceController::class, 'show'])->nam
 Route::get('/resource/edit/{resource}', [ResourceController::class,'edit'])->name('resource.edit');
 Route::put('/resource/update/{resource}',[ResourceController::class, 'update'])->name('resource.update');
 Route::delete('/resource/destroy/{resource}',[ResourceController::class, 'destroy'])->name('resource.destroy');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::get('/type', [TypeController::class,'index'])->name('type.index');
+Route::get('/type/create', [TypeController::class,'create'])->name('type.create');
+Route::post('/type/store', [TypeController::class,'store'])->name('type.store');
+Route::get('/type/show/{type}',[TypeController::class, 'show'])->name('type.show');
+Route::get('/type/edit/{type}', [TypeController::class,'edit'])->name('type.edit');
+Route::put('/type/update/{type}',[TypeController::class, 'update'])->name('type.update');
+Route::delete('/type/destroy/{type}',[TypeController::class, 'destroy'])->name('type.destroy');
