ad8572b635a13d8d34f384e7d157799fcb003161
Angela <antunezangela89@gmail.com>
2024-02-18 21:12:20 +0100

    Create ExtraResourceRequest and import it in its controller

--- app/Http/Controllers/ExtraResourceController.php
+++ app/Http/Controllers/ExtraResourceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ExtraResourceRequest;
 use App\Http\Controllers\Controller;
 use App\Models\ExtraResource;
 use Illuminate\Contracts\View\View;
