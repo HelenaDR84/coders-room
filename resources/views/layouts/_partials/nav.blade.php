42c0d3653ad9c64cca9ca3048f1a593debcad460
NathaRuiz <nruizm98@gmail.com>
2024-02-19 21:15:34 +0100

    add logo

--- resources/views/layouts/_partials/nav.blade.php
+++ resources/views/layouts/_partials/nav.blade.php
@@ -1,16 +1,16 @@
 <header>
-    <nav class="bg-violet-800 p-4">
+    <nav class="bg-purple-800 p-4">
         <div class="container mx-auto flex items-center justify-between">
             <a href="#" class="text-white text-lg font-bold">
-                <img src="ruta/al/logo.png" alt="Logo" class="w-8 h-8 mr-2 inline">
-                MyLogo
+                <img src="{{ asset('images/LogoCodersRoom.png') }}" alt="Logo" class="w-14 h-14 mr-2 inline rounded-md">
+                Coder's Room
             </a>
     
             <div class="space-x-4">
-                <a href="{{ route('resource.index') }}" class="text-white">Resources</a>
-                <a href="{{ route('type.index') }}" class="text-white">Types</a>
-                <a href="{{ route('category.index') }}" class="text-white">Categories</a>
-                <a href="{{ route('extraResource.index') }}" class="text-white">Extra Resources</a>
+                <a href="{{ route('resource.index') }}" class="text-white font-semibold">Resources</a>
+                <a href="{{ route('type.index') }}" class="text-white font-semibold">Types</a>
+                <a href="{{ route('category.index') }}" class="text-white font-semibold">Categories</a>
+                <a href="{{ route('extraResource.index') }}" class="text-white font-semibold">Extra Resources</a>
             </div>
         </div>
     </nav>
