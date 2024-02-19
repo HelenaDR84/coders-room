3a53d777047a78359d20310421729a0aef5e1301
Helena de Rosa <149380974+HelenaDR84@users.noreply.github.com>
2024-02-20 00:38:45 +0100

    add styles to typeEdit.blade.php

--- resources/views/typeEdit.blade.php
+++ resources/views/typeEdit.blade.php
@@ -1,22 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Edit Type</title>
-</head>
-<body>
-    <form action="{{ route('type.update',$type->id)}}" method="POST">
+@extends('layouts.htmlStructure')
+
+@section('title', 'Update Type')
+
+@section('content')
+
+    <form action="{{ route('type.update',$type->id)}}" method="POST" class="mx-auto w-2/4 bg-purple-800 p-8 rounded-lg shadow-md mt-5">
         @csrf
         @method('PUT')
-    <label for="title">Title:</label>
+    <label for="title" class="block text-md font-semibold text-white">Title:</label>
     <input type="text" name="title"  value="{{$type->title}}">
     @error('title')
     <div class="text-danger">{{ $message }}</div>
     @enderror
-    <button type="submit" class="">Update</button>
-        <a href="{{ route('type.index') }}" class="">Cancel</a>
+    <button type="submit" class="bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 text-white font-semibold rounded-lg text-sm px-5 py-2.5 text-center">Update</button>
+        <a href="{{ route('type.index') }}" class="mt-2 bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white font-semibold rounded-lg text-sm px-5 py-2.5 text-center inline-block">Cancel</a>
     </form> 
-</body>
-</html>
+    @endsection
