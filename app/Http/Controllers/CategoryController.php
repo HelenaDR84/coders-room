39b8a35cef603f34f007bb0fc2973c101c658aa4
maymurph <airam.g.jordan@gmail.com>
2024-02-19 13:17:56 +0100

    Add Edit category funtion

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -47,9 +47,10 @@ class CategoryController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($category)
     {
-        //
+        $category = Category::find($category);
+        return view('categoryEdit', compact('category'));
     }
 
     /**
